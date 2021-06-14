83a218781a308a58d40471389945861405133cc8
Krishnapriya S <kpriya102000@gmail.com>
2021-06-14 14:07:05 +0530

    Merged landing page to login page

--- FORUM/app1/urls.py
+++ FORUM/app1/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('landing_page/', views.landing, name='landing_page'),
 ]
--- FORUM/app1/views.py
+++ FORUM/app1/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request,'signup.html')
+
+def landing(request):
+    return render(request,'index.html')
